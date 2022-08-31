6eca1cabec8666f4d0619bf9fc9559cc3033915a
SethFarrell <sethfarell@yahoo.com>
2022-08-31 23:37:43 +0000

    corrected date time of tracks

--- scripts/bag_to_gpx.py
+++ scripts/bag_to_gpx.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from datetime import datetime
+import os
+
 import rospy
 from sensor_msgs.msg import TimeReference
 from sensor_msgs.msg import NavSatFix
@@ -12,33 +15,37 @@ class GPX_Listener:
 
     def gps_callback(self, msg):
         self.gps_data = msg
-
-    def time_callback(self, msg):
-        self.gps_time = msg
+        self.format_gpx()
 
     def format_gpx(self):
-        # convert gps data and time into trkpt line
-        time = self.gps_time.time_ref
+        date_time = datetime.fromtimestamp(self.gps_data.header.stamp.secs)
+        stamped_date = date_time.strftime('%Y-%m-%d')
+        stamped_time = date_time.strftime('%H:%M:%S')
+        time = stamped_date + 'T' + stamped_time + 'Z'
+
         lat = self.gps_data.latitude
         lon = self.gps_data.longitude
         ele = self.gps_data.altitude
-        
-        sentence = "<trkpt lat=\"{lat}\" lon=\"{lon}\"><ele>{ele}</ele><time>{time}</time></trkpt>\n".format(lat = lat, lon = lon, ele = ele, time = time)
+
+        # Format each "trkpt" using data from the NavSatFix Message 
+        sentence = "\t<trkpt lat=\"{lat}\" lon=\"{lon}\"><ele>{ele}</ele><time>{time}</time></trkpt>\n".format(lat = lat, lon = lon, ele = ele, time = time)
         f.write(sentence)
 
+    # Print the start of the gpx file (this only happens once per file)
     def print_beginning(self, date):
         f.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>\n")
         f.write("<gpx \n xmlns=\"http://www.topografix.com/GPX/1/1\"  \n creator= \"A-GPS Tracker5.5\" \n version= \"1.1\" \n xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"  \n xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\">\n")
         f.write("<metadata>\n")
     
-        sentence = "<name>" + date + "</name>\n"
+        sentence = "\t<name>" + date + "</name>\n"
         f.write(sentence)
-        f.write("<desc></desc>\n")
+        f.write("\t<desc></desc>\n")
         f.write("</metadata>\n\n")
-        f.write("<trk> \n <name></name> \n <desc></desc> \n <trkseg>\n")
+        f.write("<trk>\n\t<name></name>\n\t<desc></desc>\n\n<trkseg>\n")
 
+    # Print the ending of the gpx file (this only happens once per file)
     def print_ending(self):
-	f.write(" </trkseg>\n")
+	f.write("</trkseg>\n\n")
         f.write("</trk>\n")
         f.write("</gpx>\n")
 
@@ -49,18 +56,25 @@ if __name__ == '__main__':
     gpx_converter = GPX_Listener()
 
     rospy.Subscriber('/fix', NavSatFix, gpx_converter.gps_callback)
-    rospy.Subscriber('/time_reference', TimeReference, gpx_converter.time_callback)
 
     # Open a new .gpx file with name based on current date
-    # Add general information
+    # Create a file using the current day as file name (would be better if this was retrieved from the first message timestamp
     current_time = datetime.now()
     file_name = current_time.strftime('%m-%d-%Y')
 
     # Inside callbacks, fill in trkpt segments as we get data
-    # callbacks are called as long as rospy spins, this ends automatically when nodes shut down or maybe if our bag data ends? 
-    with open(file_name + '.gpx', 'w+') as f:
+    # callbacks are called as long as rospy spins, this ends automatically when nodes shut down or maybe if our bag data ends? Might have to manually shutdown at that point 
+    #with open("./" + file_name + '.gpx', 'w+') as f:
+        #gpx_converter.print_beginning(file_name)
+        #rospy.spin()
+        #gpx_converter.print_ending()
+
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+    with open(os.path.join(__location__, file_name + '.gpx'), 'w+') as f:
         gpx_converter.print_beginning(file_name)
         rospy.spin()
         gpx_converter.print_ending()
-        # add ending data to file before it closes
+
 
---
+++ src/bag_to_gpx.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include "ros/ros.h"
+#include "sensor_msgs/NavSatFix"
+#include "sensor_msgs/TimeReference"
+
+
+
+
+
+
+
+int main(int argc, char** argv)
+{
+ 
